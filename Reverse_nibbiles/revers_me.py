f9eed67d9072318349205411c6b9b0ea508e25b5
Tejendra <tejasbangalore@protonmail.com>
2020-12-09 22:09:19 +0530

    Finished

--- Reverse_nibbiles/revers_me.py
+++ Reverse_nibbiles/revers_me.py
@@ -1,20 +1,13 @@
+import os
+
 def export_hex():
     with open("reverse","rb") as infile, open("dumped","w") as outfile:
         hexdata = infile.read().hex()
-        outfile.write(hexdata)
+        outfile.write(hexdata[::-1])
         infile.close()
         outfile.close()
 
-def strrev(x):
-  return x[::-1]
-
-def reverse_hex():
-    with open("dumped","r") as infile, open("image.png","w") as outfile:
-        data = infile.read()
-        for i in range(len(data)):
-            temp = strrev(data[i:i+2])
-            outfile.write(temp)
-
 if __name__ == "__main__":
     export_hex()
-    reverse_hex()
+    os.popen("python -m hexdump --restore dumped > image.png")
+    
